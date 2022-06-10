309b3d74b21b10dd730d47728c3c8433a89b475a
ableenk <ableenk@protonmail.com>
2022-06-10 12:41:30 +0300

    includes file

---
+++ cpp/include.h
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <string>
+#include <iomanip>
+#include <map>
+#include <utility>
+#include <vector>
+
+using namespace std;
+
+pair<float, float> lr(const vector<pair<int, int>> coords);
