f59ba8239a5022acae4c10ac0c13203d9ddd59f0
ableenk <ableenk@protonmail.com>
2022-06-11 13:23:13 +0300

    updated code

--- python visualize/visualize.py
+++ python visualize/visualize.py
@@ -3,33 +3,103 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 import numpy as np
 
-print("Enter the amount of points")
-n = int(input())
+class Converter():
+    '''Converting utility.
 
-Xs = []
-Ys = []
+    '''
+    def __init__(self):
+        pass
 
-print("Enter coordinates")
-coords = []
-for i in range(n):
-    x, y = map(int, input().split())
-    coords += [(x, y)]
-    Xs += [x]
-    Ys += [y]
+    def convert_scatter_data(self, points: list, names: dict=None) -> pd.DataFrame():
+        '''Convert abstract plot data to pandas dataframe.
+            
+        Args:
+            point (list[tuple]): plots points.
 
-print("Enter linear coefficients")
-b, a = map(float, input().split())
-line_x = np.linspace(0,10,100)
-line_y = b + a*line_x
+        Kwargs:
+            names (tuple): axises names for dataframe.
 
-scatterdata = pd.DataFrame({'x': Xs, 'y': Ys})
-linedata = pd.DataFrame({'x': line_x, 'y': line_y})
+        Return:
+            data (pd.DataFrame): converted data.
 
-sns.set_theme(style="darkgrid")
-sns.color_palette('pastel')
-_, plots = plt.subplots(1, 1, figsize=(8, 8))
-first_graph = sns.scatterplot(x='x', y='y', data=scatterdata, ax=plots, color='r')
-first_graph.set_xticks(np.arange(0, 11))
-first_graph.set_yticks(np.arange(0, 11))
-second_graph = sns.lineplot(x='x', y='y', data=linedata, ax=plots, color='b')
-plt.show()
+        '''
+        x_array = []
+        y_array = []
+        for point in points:
+            x_array.append(point[0])
+            y_array.append(point[1])
+        data = pd.DataFrame({names['x']: x_array, names['y']: y_array})
+        return data
+
+class Visualizer():
+    '''Utility for visualizing modeling data.
+
+    '''
+    def __init__(self, style: str="darkgrid", palette: str="pastel", figsize: tuple=(8, 8), 
+                                        space: tuple=(0, 10), names: tuple=('x', 'y'), title: str=None):
+        sns.set_theme(style=style)
+        sns.color_palette(palette)
+        self.plot = plt.plot(figsize=figsize)
+        self.conv = Converter()
+        self.space = space
+        self.names = {'x': names[0], 'y': names[1]}
+        self.title = title
+        plt.title(title)
+
+    def lineplot_by_equation(self, b, a, color='b'):
+        '''Show linear equation on the graph.
+
+        Args:
+            b (int): first coefficient.
+            a (int): first coefficient.
+        
+        Kwargs:
+            color (str): plot's color.
+
+        '''
+        data_x = np.linspace(self.space[0], self.space[1], 100)
+        data_y = b + a*data_x
+        data = pd.DataFrame({self.names['x']: data_x, self.names['y']: data_y})
+        sns.lineplot(x=self.names['x'], y=self.names['y'], data=data, color=color).set_title(self.title)
+    
+    def scatterplot_by_points(self, points, color='r'):
+        '''Show scatterplot by points on the graph.
+
+        Args:
+            points (list[tuple]): points of the graph.
+        
+        Kwargs:
+            color (str): plot's color.
+
+        '''
+        data = self.conv.convert_scatter_data(points, names=self.names)
+        plot = sns.scatterplot(x=self.names['x'], y=self.names['y'], data=data, color=color)
+        ticks = np.arange(self.space[0], self.space[1]+1)
+        plot.set_xticks(ticks)
+        plot.set_yticks(ticks)
+        plot.set_title(self.title)
+
+    @staticmethod
+    def show():
+        '''Show plot.
+        
+        '''
+        plt.show()
+
+def main():
+    print("Enter the amount of points")
+    n = int(input())
+    print("Enter coordinates")
+    coords = []
+    for i in range(n):
+        x, y = map(int, input().split())
+        coords.append((x, y))
+    print("Enter linear coefficients")
+    b, a = map(float, input().split())
+    v = Visualizer(space=(0, 5))
+    v.lineplot_by_equation(b, a)
+    v.scatterplot_by_points(coords)
+    v.show()
+
+if __name__ == '__main__':
+    main()
